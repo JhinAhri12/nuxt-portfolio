78b753de41606e52a15ba64fee6dec83283d93c7
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-02-28 16:18:58 +0100

    feat: finish description of project

--- components/Project.vue
+++ components/Project.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="row">
-        <div class="card col-sm-12 card-projet" style="width: 20rem;">
+    <div class="row mx-auto">
+        <div class="card col-sm-12 col-md-3 card-projet mb-4 me-2" >
             <img src="../assets/img/project1.jpg" class="card-img-top " alt="projet">
-            <div class="card-body">
+            <div class="card-body ">
                 <h3 class="card-title">Nuxt Job</h3>
                 <p class="card-text">Plateforme pour trouver le job idéal sur la techno nuxt !</p>
                 <a href="#" class="btn btn-primary btn-lg">Voir le projet</a>
             </div>
         </div>
-        <div class="card col-sm-12 card-projet" style="width: 20rem;">
+        <div class="card col-sm-12 col-md-4 card-projet mb-4 me-2">
             <img src="../assets/img/project1.jpg" class="card-img-top" alt="projet">
             <div class="card-body">
                 <h3 class="card-title">Nuxt Disney</h3>
@@ -16,11 +16,35 @@
                 <a href="#" class="btn btn-primary btn-lg">Voir le projet</a>
             </div>
         </div>
+        <div class="card col-sm-12 col-md-4 card-projet mb-4">
+            <img src="../assets/img/project1.jpg" class="card-img-top" alt="projet">
+            <div class="card-body">
+                <h3 class="card-title">Nuxt Plants</h3>
+                <p class="card-text">Site web qui permet d'afficher des informations sur des plantes selon une api</p>
+                <a href="#" class="btn btn-primary btn-lg">Voir le projet</a>
+            </div>
+        </div>
+        <div class="card col-sm-12 col-md-4 card-projet mb-4">
+            <img src="../assets/img/project1.jpg" class="card-img-top" alt="projet">
+            <div class="card-body">
+                <h3 class="card-title">Nuxt Blog</h3>
+                <p class="card-text">Exemple de Blog nuxt avec Nuxtcontent</p>
+                <a href="#" class="btn btn-primary btn-lg">Voir le projet</a>
+            </div>
+        </div>
+        <div class="card col-sm-12  col-md-4 card-projet mb-4">
+            <img src="../assets/img/project1.jpg" class="card-img-top" alt="projet">
+            <div class="card-body">
+                <h3 class="card-title">Nuxt Currency</h3>
+                <p class="card-text">Site web qui affiche des informations sur une cryptomonaie selon une api</p>
+                <a href="#" class="btn btn-primary btn-lg">Voir le projet</a>
+            </div>
+        </div>
     </div>
 </template>
 <style scoped>
 .card-projet{
-    margin-left: 15px;
-    margin-bottom: 15px
+    width : 18 rem;
+
 }
 </style>
