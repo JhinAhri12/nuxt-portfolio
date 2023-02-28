f0a5b31bfcc4f65b42db39704f28444ad3e33df7
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-02-28 15:38:35 +0100

    feat: Minimal Banner and call to action

---
+++ components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="banner">
+        <div class="container">
+            <h1><strong>Bienvenue sur mon portfolio</strong>!</h1>
+            <p>Découvrez mes projets en cours.</p>
+            <a class="btn btn-flat" href="#">Je découvre les projets</a>
+        </div>
+    </div>
+</template>
+<style scoped>
+.banner {
+  background-size: cover;
+  background-position: center;
+  padding: 150px 0;
+  background-image: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('./assets/img/banner.jpg')
+}
+
+.banner h1 {
+  margin: 0;
+  color: white;
+  text-shadow: 1px 1px 3px rgba(0,0,0,0.2);
+  font-size: 32px;
+  font-weight: bold;
+}
+
+.banner p {
+  font-size: 20px;
+  color: white;
+  opacity: .7;
+  text-shadow: 1px 1px 3px rgba(0,0,0,0.2);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <Banner/>
+        <div class="container">
+
+        </div>
+</template>
