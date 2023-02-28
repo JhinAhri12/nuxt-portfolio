4c0ec6d09d7d5729eb73647e104fa639785ba48e
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-02-28 16:00:36 +0100

    feat: minimal Card and description of project

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <Banner/>
         <div class="container">
-
+            <h2> Mes projets : </h2>
+            <Project />
+            <h2> Contact :</h2>
         </div>
 </template>
